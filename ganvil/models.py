713f69a340ceb02f3d963a1644ff64484649d8cd
Lindsey Lawler <lawlerlindsey@gmail.com>
2022-12-28 19:24:11 -0500

    more models added, not migrated as they need to be looked over first

--- ganvil/models.py
+++ ganvil/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+# specifying choices
+
+RARITY_CHOICES = (
+    ("common", "Common"),
+    ("uncommon", "Uncommon"),
+    ("rare", "Rare"),
+    ("very_rare", "Very Rare"),
+    ("legendary", "Legendary"),
+    ("artifact", "Artifact"),
+    ("unknown", "Unknown Rarity"),
+    ("vary", "Varies")
+)
+
+
 # Create your models here.
 
 class User(models.Model):
@@ -11,3 +25,20 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, null=False, blank = False)
+    rarity = models.CharField(max_length=50, choices=RARITY_CHOICES, default="common")
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    attunement = models.BooleanField()
+    image = models.ImageField(upload_to='uploads/images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='cart')
+    total = models.DecimalField(max_digits=12, decimal_places=2)
