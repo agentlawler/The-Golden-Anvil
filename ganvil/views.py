517c61be48208f676d1cdf7a7430dfb97324c08e
Lindsey Lawler <lawlerlindsey@gmail.com>
2022-12-28 19:47:02 -0500

    views.py updated

--- ganvil/views.py
+++ ganvil/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 from .models import User, Item, Cart
+from .serializers import UserSerializer, ItemSerializer, CartSerializer
+from rest_framework import viewsets
 # Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ItemView(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class CartView(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
